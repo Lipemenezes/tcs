e9c41164a9697d496efe1ce3d9c0a2411174fe44
Felipe Menezes <lipe.menezes@live.com>
2017-12-03 17:40:51 -0200

    Added Postman Web Service tests

--- src/entity/Avaliacao.java
+++ src/entity/Avaliacao.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -43,8 +44,7 @@ public class Avaliacao {
 	@Column
 	private boolean ativo;
 
-	@OneToOne
-	@JoinColumn(name = "turma_id")
+	@OneToOne(fetch = FetchType.EAGER)
 	private Turma turma;
 
 	public Avaliacao() {
